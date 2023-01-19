60c0431c9f130a5425b1dab5d6ba4b27cf38fcec
raymondchooi <raymond_chooi282@hotmail.com>
2023-01-19 17:53:20 -0500

    Change editor to just save button

--- src/components/common/Editor.tsx
+++ src/components/common/Editor.tsx
@@ -22,7 +22,7 @@ const Editor = ({
 	const editorJS = useRef(null);
 
 	const [data, setData] = useState("");
-	const [editMode, setEditMode] = useState(false);
+	const [hasChanges, setHasChanges] = useState(false);
 
 	// Dummy data
 	//`{"time":1674009351098,"blocks":[{"id":"lLg8bWk7VH","type":"header","data":{"text":"Start typing...","level":1}}],"version":"2.26.4"}`
@@ -38,17 +38,22 @@ const Editor = ({
 	const handleSaveClick = useCallback(async () => {
 		const savedData = await editorJS?.current?.save();
 		saveData && saveData(JSON.stringify(savedData));
-		setEditMode(false);
+		setHasChanges(false);
 	}, [saveData]);
 
-	const handleEditClick = () => !readOnly && setEditMode(true);
-	const handleCancelClick = () => !readOnly && setEditMode(false);
+	const handleEditClick = () => {
+		!readOnly && setEditMode(true);
+	};
+	const handleCancelClick = () => {
+		!readOnly && setEditMode(false);
+	};
 
 	const handleChange = useCallback(async () => {
-		const savedData = await editorJS?.current?.save();
-		onChange && onChange(JSON.stringify(savedData));
-		setData(savedData);
-	}, [onChange]);
+		setHasChanges(true);
+		// const savedData = await editorJS?.current?.save();
+		// onChange && onChange(JSON.stringify(savedData));
+		// setData(savedData);
+	}, []);
 
 	// https://github.com/Jungwoo-An/react-editor-js/issues/200
 	const updateValue = useCallback((data) => {
@@ -60,18 +65,7 @@ const Editor = ({
 	return (
 		<div className="content">
 			<div>
-				{!readOnly && !editMode ? (
-					<div className="mt-6 flex justify-center items-center w-full gap-4">
-						<button
-							onClick={handleEditClick}
-							type="button"
-							className="text-xs font-light inline-flex items-center rounded-lg border border-gray-300 bg-white py-1.5 px-3 text-black shadow-sm hover:bg-indigo-500 hover:border-indigo-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-						>
-							Edit
-						</button>
-					</div>
-				) : null}
-				{editMode ? (
+				{!readOnly && hasChanges ? (
 					<div className="mt-6 flex justify-center items-center w-full gap-4">
 						<button
 							onClick={handleSaveClick}
@@ -80,18 +74,12 @@ const Editor = ({
 						>
 							Save
 						</button>
-						<button
-							onClick={handleCancelClick}
-							type="button"
-							className="text-xs font-light inline-flex items-center rounded-lg border border-gray-300 bg-white py-1.5 px-3 text-black shadow-sm hover:bg-indigo-500 hover:border-indigo-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-						>
-							Cancel
-						</button>
 					</div>
 				) : null}
 			</div>
 			<ReactEditorJS
-				readOnly={readOnly || !editMode}
+				onChange={handleChange}
+				readOnly={readOnly}
 				enableReInitialize={true}
 				onInitialize={handleInitialize}
 				tools={EDITOR_JS_TOOLS}
